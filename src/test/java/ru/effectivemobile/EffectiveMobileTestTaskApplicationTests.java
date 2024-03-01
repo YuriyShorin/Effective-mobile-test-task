c00149db7f980da8e858aa150112f93c89d6900b
YuriyShorin <ushorin.03@gmail.com>
2024-03-01 18:52:56 +0300

    feat: app

---
+++ src/test/java/ru/effectivemobile/EffectiveMobileTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effectivemobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveMobileTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
